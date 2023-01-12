b8537603ac3be8c3d49b6ca4b9115b723cf66191
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-01-11 18:32:33 -0800

    1-21

--- exe_synth.py
+++ exe_synth.py
@@ -348,7 +348,7 @@ models = {
     # 'SAITS': saits,
     'DiffSAITS': model_diff_saits
 }
-mse_folder = "results_mse_synth"
+mse_folder = "results_mse_synth_1"
 
 lengths = [20]#[10, 25, 40, 45]
 print("For All")
