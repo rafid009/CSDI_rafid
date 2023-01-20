8f099435d8e964e4c6e1c879b7dcc5d774c1d023
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-01-20 04:47:13 -0800

    mean along with median 1-5

--- utils.py
+++ utils.py
@@ -461,7 +461,7 @@ def evaluate_imputation(models, mse_folder, exclude_key='', exclude_features=Non
 
                         mse_diff_saits = ((samples_diff_saits_mean[0, :, feature_idx] - c_target[0, :, feature_idx]) * eval_points[0, :, feature_idx]) ** 2
                         mse_diff_saits = mse_diff_saits.sum().item() / eval_points[0, :, feature_idx].sum().item()
-                        if feature not in mse_diff_saits_total.keys():
+                        if "mean" not in mse_diff_saits_total.keys():
                             mse_diff_saits_total[feature] = {"mean": mse_diff_saits}
                         else:
                             mse_diff_saits_total[feature]["mean"] += mse_diff_saits
