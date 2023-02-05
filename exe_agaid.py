a9639767ed2ca7bfbf9d62bc5305ecc0a178cffb
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-02-04 19:06:11 -0800

    diffsaits final debug 1-7

--- exe_agaid.py
+++ exe_agaid.py
@@ -82,7 +82,7 @@ config_dict_diffsaits = {
     'train': {
         'epochs': 2500,
         'batch_size': 16 ,
-        'lr': 1.0e-3
+        'lr': 2.0e-4
     },      
     'diffusion': {
         'layers': 4, 
