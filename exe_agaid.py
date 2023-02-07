e051b16f00f01042d55656b0caa061bc20e93e20
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-02-06 21:51:19 -0800

    diffsaits final debug 1-24 season 33 mix nl4 nh4

--- exe_agaid.py
+++ exe_agaid.py
@@ -86,13 +86,13 @@ config_dict_diffsaits = {
         'lr': 1.0e-3
     },      
     'diffusion': {
-        'layers': 3, 
+        'layers': 4, 
         'channels': 64,
         'nheads': 8,
         'diffusion_embedding_dim': 128,
         'beta_start': 0.0001,
         'beta_end': 0.5,
-        'num_steps': 70,
+        'num_steps': 60,
         'schedule': "quad"
     },
     'model': {
@@ -106,7 +106,7 @@ config_dict_diffsaits = {
         'n_feature': len(features),
         'd_model': 256,
         'd_inner': 128,
-        'n_head': 3,
+        'n_head': 4,
         'd_k': 64,
         'd_v': 64,
         'dropout': 0.1,
