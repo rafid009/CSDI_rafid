f2425325750f480bbf64330c98ff6562981962ff
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-02-09 11:46:28 -0800

    diffsaits final season 33 changes made residual added 1-2 gelu, gate, filter lr schedule added

--- exe_agaid.py
+++ exe_agaid.py
@@ -122,14 +122,14 @@ config_info = 'model_diff_saits_final_stack.pth'
 
 model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 
-# train(
-#     model_diff_saits,
-#     config_dict_diffsaits["train"],
-#     train_loader,
-#     valid_loader=valid_loader,
-#     foldername=model_folder,
-#     filename=f"{filename}"
-# )
+train(
+    model_diff_saits,
+    config_dict_diffsaits["train"],
+    train_loader,
+    valid_loader=valid_loader,
+    foldername=model_folder,
+    filename=f"{filename}"
+)
 # nsample = 100
 # model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 # model_diff_saits_simple.load_state_dict(torch.load(f"{model_folder}/{filename_simple}"))
