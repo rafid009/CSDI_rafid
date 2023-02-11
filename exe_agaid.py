d7d627cbc93c57727b71c7bf4cb6b00785a55796
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-02-10 21:31:58 -0800

    diffsaits final season 33 changes made residual added 1-2 gelu, gate, filter lr schedule added lr=2e-3, 3 heads 4 nlayer

--- exe_agaid.py
+++ exe_agaid.py
@@ -83,7 +83,7 @@ config_dict_diffsaits = {
     'train': {
         'epochs': 3000,
         'batch_size': 16 ,
-        'lr': 9.0e-3
+        'lr': 2.0e-3
     },      
     'diffusion': {
         'layers': 4, 
