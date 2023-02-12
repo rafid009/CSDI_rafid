e141763f5c04d6bf98aee22575180294065409b9
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-02-12 11:58:40 -0800

    diffsaits final season 33 changes made residual added 1-2 gelu, gate, filter lr schedule added cosine warm restart T0=500, T_mult=2, min=1.0e-5 per batch, 3 heads 4 nlayer, Gated activation, X+X_tilde mix

--- diff_models.py
+++ diff_models.py
@@ -167,8 +167,8 @@ class ResidualEncoderLayer(nn.Module):
                          diagonal_attention_mask)
         self.pre_mid_projection = Conv1d_with_init(channels, channels, 1)
         self.mid_projection = Conv1d_with_init(channels, 2, 1)
-        self.output_projection = Conv1d_with_init(1, 4, 1)
-        # self.output_projection = Conv1d_with_init(channels, 4, 1)
+        # self.output_projection = Conv1d_with_init(1, 4, 1)
+        self.output_projection = Conv1d_with_init(channels, 4, 1)
         self.diffusion_projection = nn.Linear(diffusion_embedding_dim, channels)
         self.init_projection = Conv1d_with_init(2, channels, 1)
         # Transposed
