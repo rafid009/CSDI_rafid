ce2a31751cabb164d541bbf96165fbd9bc20bfac
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-02-15 21:21:34 -0800

    diffsaits final season 33 changes made residual added 1-2 gelu, gate, filter lr schedule added multi-step per epoch, 3 heads 4 nlayer, new_high, 2 encoders, softmax sttn_weights

--- diff_models.py
+++ diff_models.py
@@ -345,7 +345,7 @@ class ResidualEncoderLayer(nn.Module):
         residual, skip = torch.chunk(y, 2, dim=1)
         # y = torch.sigmoid(slice_X) * torch.tanh(slice_eps)
         residual = residual.reshape(base_shape)
-        skip = self.out_skip_proj(skip)
+        skip = F.gelu(self.out_skip_proj(skip))
         skip = skip.reshape(B, K, L)
         # print(f"attn weight: {attn_weights_1.shape}")
 
@@ -358,7 +358,7 @@ class ResidualEncoderLayer(nn.Module):
         attn_weights_2 = attn_weights_2.reshape((B, -1, attn_shape_2[1], attn_shape_2[2], attn_shape_2[3]))
         attn_weights_2 = torch.mean(attn_weights_2, dim=1)
         # print(f"attn weight: {attn_weights.shape}")
-        attn_weights = attn_weights_1 + attn_weights_2
+        attn_weights = torch.sigmoid(attn_weights_1 + attn_weights_2, dim=-1)
         # return (x + residual) / math.sqrt(2.0), skip, attn_weights
         return (x + residual), skip, attn_weights
 
