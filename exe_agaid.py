ece96ede4e6af7e6443654156db59585a446d99f
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-02-22 11:20:51 -0800

    cov1D input shape corrected, remove attn_2, removed sogmoid from attn_weight

--- exe_agaid.py
+++ exe_agaid.py
@@ -92,7 +92,7 @@ config_dict_diffsaits = {
         'diffusion_embedding_dim': 128,
         'beta_start': 0.0001,
         'beta_end': 0.5,
-        'num_steps': 50,
+        'num_steps': 70,
         'schedule': "quad"
     },
     'model': {
