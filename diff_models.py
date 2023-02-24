c9de277eecde75ff18d5a51baceecdc927749e93
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-02-24 14:13:12 -0800

    cov1D input shape corrected, remove attn_2, removed sogmoid from attn_weight, removed weight norm, csdi train enabled, num_steps=70, tryimg out the new thing 1-5

--- diff_models.py
+++ diff_models.py
@@ -801,10 +801,9 @@ class diff_SAITS_2(nn.Module):
         diff_emb = self.diffusion_embedding(diffusion_step)
 
         pos_cond = self.position_enc_cond(cond)
-
-
-        skips_tilde_1 = torch.zeros_like(noise)
+        
         enc_output = self.dropout(self.position_enc_noise(noise))
+        skips_tilde_1 = torch.zeros_like(enc_output)
         for encoder_layer in self.layer_stack_for_first_block:
             enc_output, skip, _ = encoder_layer(enc_output, pos_cond, diff_emb)
             print(f"skip: {skip.shape}")
@@ -831,8 +830,9 @@ class diff_SAITS_2(nn.Module):
 
         # pos_cond = self.position_enc_cond(cond)
 
-        skips_tilde_2 = torch.zeros_like(noise)
+        # skips_tilde_2 = torch.zeros_like(noise)
         enc_output = self.position_enc_noise(noise)
+        skips_tilde_2 = torch.zeros_like(enc_output)
         for encoder_layer in self.layer_stack_for_second_block:
             enc_output, skip, attn_weights = encoder_layer(enc_output, pos_cond, diff_emb)
             skips_tilde_2 += skip
