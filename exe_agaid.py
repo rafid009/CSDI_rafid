d88ac6fc27fbe06956bf9d5b929366ec6079a8a5
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-02-28 11:34:47 -0800

    cov1D input shape corrected, remove attn_2, removed sogmoid from attn_weight, removed weight norm, csdi train enabled, num_steps=70, tryimg out the new thing 1-27

--- exe_agaid.py
+++ exe_agaid.py
@@ -91,7 +91,7 @@ config_dict_diffsaits = {
         'nheads': 8,
         'diffusion_embedding_dim': 128,
         'beta_start': 0.0001,
-        'beta_end': 0.3,
+        'beta_end': 0.5,
         'num_steps': 70,
         'schedule': "quad"
     },
