e0af1d2dcfd9f3239388837e93868344fc68244a
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-03-01 16:05:41 -0800

    cov1D input shape corrected, remove attn_2, removed sogmoid from attn_weight, removed weight norm, csdi train enabled, num_steps=70, tryimg out the new thing 1-28......mask[1]=mask[0], then x_tilde_1=x_tilde_1*(1-mask)+X[1], no feature attn, lr scheduler intact, separated cond embedding initially, 1000 more epochs

--- exe_agaid.py
+++ exe_agaid.py
@@ -81,7 +81,7 @@ model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
     
 config_dict_diffsaits = {
     'train': {
-        'epochs': 3000,
+        'epochs': 1000,
         'batch_size': 16 ,
         'lr': 1.0e-3
     },      
@@ -120,8 +120,8 @@ model_diff_saits = CSDI_Agaid(config_dict_diffsaits, device, is_simple=False).to
 filename = 'model_diff_saits_final_stable_new_high.pth'
 config_info = 'model_diff_saits_final_stack.pth'
 
-# model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
-
+model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
+# 
 train(
     model_diff_saits,
     config_dict_diffsaits["train"],
