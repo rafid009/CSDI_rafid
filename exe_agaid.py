7e87b885a02465a38787702ead8ac349874be765
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-03-01 18:04:26 -0800

    cov1D input shape corrected, remove attn_2, removed sogmoid from attn_weight, removed weight norm, csdi train enabled, num_steps=70, tryimg out the new thing 1-28......mask[1]=mask[0], then x_tilde_1=x_tilde_1*(1-mask)+X[1], no feature attn, lr scheduler intact, separated cond embedding initially, 1000 more epochs 1-1

--- exe_agaid.py
+++ exe_agaid.py
@@ -83,7 +83,7 @@ config_dict_diffsaits = {
     'train': {
         'epochs': 1000,
         'batch_size': 16 ,
-        'lr': 1.0e-3
+        'lr': 1.0e-4
     },      
     'diffusion': {
         'layers': 4, 
