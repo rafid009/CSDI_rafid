34427c724140bddd9a6bc6daf3d27bc61ccb4ce6
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-03-08 18:50:31 -0800

    Added feature correlation mapping 1-5, lr=1e-4

--- exe_agaid.py
+++ exe_agaid.py
@@ -83,7 +83,7 @@ config_dict_diffsaits = {
     'train': {
         'epochs': 3000,
         'batch_size': 16 ,
-        'lr': 1.0e-2
+        'lr': 1.0e-4
     },      
     'diffusion': {
         'layers': 4, 
