2749704dd93e30cf89d887e9b072f26e872ec55c
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-03-14 17:47:56 -0700

    synthetic experiments done, with feat corr, lr=1e-4, d_model=128, layer=4, diff steps=60, channels=d_model, mix, cond added before the DMHSA, lr shcedule=p3,p2, loss=predict3+2+1, beta_end=0.6

--- main_model.py
+++ main_model.py
@@ -157,11 +157,11 @@ class CSDI_base(nn.Module):
                     'missing_mask': total_mask
                 }
             predicted_1, predicted_2, predicted_3 = self.diffmodel(inputs, t)
-            # residual_1 = (noise - predicted_1) * target_mask
-            # residual_2 = (noise - predicted_2) * target_mask
+            residual_1 = (noise - predicted_1) * target_mask
+            residual_2 = (noise - predicted_2) * target_mask
             residual_3 = (noise - predicted_3) * target_mask
-            # loss = ((residual_1 ** 2).sum() + (residual_2 ** 2).sum() + (residual_3 ** 2).sum()) / (3 * (num_eval if num_eval > 0 else 1))
-            loss = (residual_3 ** 2).sum() / (num_eval if num_eval > 0 else 1)
+            loss = ((residual_1 ** 2).sum() + (residual_2 ** 2).sum() + (residual_3 ** 2).sum()) / (3 * (num_eval if num_eval > 0 else 1))
+            # loss = (residual_3 ** 2).sum() / (num_eval if num_eval > 0 else 1)
         else:
             predicted = self.diffmodel(total_input, side_info, t)  # (B,K,L)
             residual = (noise - predicted) * target_mask
