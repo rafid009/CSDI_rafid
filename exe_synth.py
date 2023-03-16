04b834830c79d48d088e3f576844cc5cceeddfed
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-03-15 17:53:13 -0700

    synthetic experiments done, with feat corr, lr=1e-4, d_model=128, layer=4, diff steps=60, channels=d_model, mix, cond added before the DMHSA, lr shcedule=p3,p2, loss=predict3, beta_end=0.5, attn_time, another c_y added before enc2, remove residula after block 1

--- exe_synth.py
+++ exe_synth.py
@@ -327,7 +327,7 @@ train(
     filename=f"{filename}",
     is_saits=True
 )
-# model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
+model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 
 saits_model_file = f"{model_folder}/saits_model_synth.pkl"
 saits = SAITS(n_steps=n_steps, n_features=n_features, n_layers=3, d_model=256, d_inner=128, n_head=4, d_k=64, d_v=64, dropout=0.1, epochs=2000, patience=200, device=device)
