0163d91085aaf627e2d12f2b55880cadc9db5861
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-03-17 03:15:06 -0700

    synthetic experiments done, with feat corr, lr=1e-4, d_model=128, layer=4, diff steps=50, channels=d_model, mix, cond added before the DMHSA, lr shcedule=p3,p2, loss=predict3, beta_end=0.5, attn_time_1 used, attn_f after attn_2 c_y added middle, dropout=0.2, nhead=4, last conv output with init zeros

--- diff_models.py
+++ diff_models.py
@@ -941,7 +941,7 @@ class diff_SAITS_2(nn.Module):
         skips_tilde_2 = torch.transpose(skips_tilde_2, 1, 2)
         skips_tilde_3 = torch.transpose(skips_tilde_3, 1, 2)
 
-        skips_tilde_3 = self.output_proj(skips_tilde_3)
+        skips_tilde_3 = F.relu(self.output_proj(skips_tilde_3))
 
         # skips_tilde_3 = self.final_conv(skips_tilde_3)
         # X_c = masks * X + (1 - masks) * X_tilde_3  # replace non-missing part with original data
