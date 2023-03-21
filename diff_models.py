24be99bae044d1474fc04878e9ab9817c161b433
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-03-21 15:16:01 -0700

    synthetic experiments done, with feat corr, lr=1e-4, d_model=128, layer=4, diff steps=50, channels=d_model, mix, cond added before the DMHSA, lr shcedule=p3,p2, loss=predict3, beta_end=0.5, attn_time 2, dropout=0.2, nhead=4, attn_f bmm with skip_tildes

--- diff_models.py
+++ diff_models.py
@@ -771,7 +771,7 @@ class ResidualEncoderLayer_2(nn.Module):
         skip = torch.transpose(skip, 1, 2) # (B, K, L)
 
 
-        attn_weights = torch.softmax(attn_weights_1 + attn_weights_2, dim=-1)
+        attn_weights = attn_weights_2#torch.softmax(attn_weights_1 + attn_weights_2, dim=-1)
         # print(f"attn: {attn_weights.shape}")
 
         return (x + residual) * math.sqrt(0.5), skip, attn_weights, attn_weights_f
@@ -864,14 +864,14 @@ class diff_SAITS_2(nn.Module):
 
         skips_tilde_1 /= math.sqrt(len(self.layer_stack_for_first_block))
         # Feature cross
-        # attn_weights_f = attn_weights_f.squeeze(dim=1)  # namely term A_hat in Eq.
-        # # print(f"attn 0: {attn_weights_f.shape}")
-        # if len(attn_weights_f.shape) == 4:
-        #     # if having more than 1 head, then average attention weights from all heads
-        #     attn_weights_f = torch.transpose(attn_weights_f, 1, 3)
-        #     attn_weights_f = attn_weights_f.mean(dim=3)
-        #     attn_weights_f = torch.transpose(attn_weights_f, 1, 2)
-        # skips_tilde_1 = skips_tilde_1 @ torch.sigmoid(attn_weights_f)
+        attn_weights_f = attn_weights_f.squeeze(dim=1)  # namely term A_hat in Eq.
+        # print(f"attn 0: {attn_weights_f.shape}")
+        if len(attn_weights_f.shape) == 4:
+            # if having more than 1 head, then average attention weights from all heads
+            attn_weights_f = torch.transpose(attn_weights_f, 1, 3)
+            attn_weights_f = attn_weights_f.mean(dim=3)
+            attn_weights_f = torch.transpose(attn_weights_f, 1, 2)
+        skips_tilde_1 = skips_tilde_1 @ torch.sigmoid(attn_weights_f)
 
         # feature corr end
         skips_tilde_1 = self.reduce_skip_z(skips_tilde_1)
