b297fbdd0f33d9f8618eb1c206f8ad7c134c0daa
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-03-22 13:39:04 -0700

    synthetic experiments done, with feat corr, lr=1e-4, d_model=128, layer=4, diff steps=50, channels=d_model, mix, cond added before the DMHSA, lr shcedule=p3,p2, loss=predict3, beta_end=0.5, attn_times softmax, dropout=0.2, nhead=4, attn_f from 2nd block bmm with skip_tildes 1-2

--- exe_synth.py
+++ exe_synth.py
@@ -343,7 +343,7 @@ config_dict_diffsaits = {
     'train': {
         'epochs': 3000,
         'batch_size': 16 ,
-        'lr': 1.0e-4
+        'lr': 1.0e-3
     },      
     'diffusion': {
         'layers': 4, 
