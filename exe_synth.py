f57d8dd462562a17a607ac2a1ffdc60ed33d0884
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-04-01 19:21:17 -0700

    attn_f outside block, noise gets orig + noisy part after attention on the features 1-5

--- exe_synth.py
+++ exe_synth.py
@@ -344,7 +344,7 @@ config_dict_diffsaits = {
     'train': {
         'epochs': 1500,
         'batch_size': 16 ,
-        'lr': 1.0e-4
+        'lr': 1.0e-3
     },      
     'diffusion': {
         'layers': 4, 
