5995299a96e69b8de3eeff24f8215475cf88d08d
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-04-02 10:27:21 -0700

    attn_f outside block, noise gets orig + noisy part after attention on the features, impute + recon loss like SAITS, lr=1e-3 1-2

--- main_model.py
+++ main_model.py
@@ -142,7 +142,7 @@ class CSDI_base(nn.Module):
         noisy_data = (current_alpha ** 0.5) * observed_data + ((1.0 - current_alpha) ** 0.5) * noise
         total_input = self.set_input_to_diffmodel(noisy_data, observed_data, cond_mask)
         target_mask = observed_mask - cond_mask
-        num_eval = observed_mask.sum() #target_mask.sum()
+        num_eval = target_mask.sum()
         if self.is_saits:
             temp_mask = cond_mask.unsqueeze(dim=1)
             if self.is_simple:
@@ -159,9 +159,15 @@ class CSDI_base(nn.Module):
             predicted_1, predicted_2, predicted_3 = self.diffmodel(inputs, t)
             # residual_1 = (noise - predicted_1) * target_mask
             # residual_2 = (noise - predicted_2) * target_mask
-            residual_3 = (noise - predicted_3) * observed_mask #target_mask
+            residual_3 = (noise - predicted_3) * target_mask
             # loss = ((residual_1 ** 2).sum() * 0.5 + (residual_2 ** 2).sum() * 0.5 + (residual_3 ** 2).sum()) / (3 * (num_eval if num_eval > 0 else 1))
             loss = (residual_3 ** 2).sum() / (num_eval if num_eval > 0 else 1)
+            recon_eval = cond_mask.sum()
+            recon_loss_1 = (noise - predicted_1) * cond_mask
+            recon_loss_2 = (noise - predicted_2) * cond_mask
+            recon_loss_3 = (noise - predicted_3) * cond_mask
+            recon_loss = ((recon_loss_1 ** 2).sum() + (recon_loss_2 ** 2).sum() + (recon_loss_3 ** 2).sum()) / (3 * (recon_eval if recon_eval > 0 else 1))
+            loss = loss + 0.5 * recon_loss
         else:
             predicted = self.diffmodel(total_input, side_info, t)  # (B,K,L)
             residual = (noise - predicted) * target_mask
