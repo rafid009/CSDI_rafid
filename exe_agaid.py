f51cbe82a371313ab5e1011b5f4c7d4362df8086
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-04-03 20:11:33 -0700

    attn_f outside block, noise gets orig + noisy part after attention on the features, impute*0.7 + recon*0.3 loss like SAITS, lr=1e-3, agaid enriched, 1-15

--- exe_agaid.py
+++ exe_agaid.py
@@ -73,7 +73,7 @@ filename = 'model_csdi.pth'
 #     filename=filename
 # )
 # nsample = 50
-# model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
+model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 # evaluate(model_csdi, valid_loader, nsample=nsample, scaler=1, foldername=model_folder)
 # model_folder_exp = "./saved_model_explode"
 # if not os.path.isdir(model_folder_exp):
@@ -122,17 +122,17 @@ config_info = 'model_diff_saits_final_stack.pth'
 
 # model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 # 
-train(
-    model_diff_saits,
-    config_dict_diffsaits["train"],
-    train_loader,
-    valid_loader=valid_loader,
-    foldername=model_folder,
-    filename=f"{filename}",
-    is_saits=True
-)
+# train(
+#     model_diff_saits,
+#     config_dict_diffsaits["train"],
+#     train_loader,
+#     valid_loader=valid_loader,
+#     foldername=model_folder,
+#     filename=f"{filename}",
+#     is_saits=True
+# )
 # nsample = 100
-# model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
+model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 
 
 filename = "ColdHardiness_Grape_Merlot_2.csv"
