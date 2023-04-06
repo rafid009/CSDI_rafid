5700f202391d11c1cccade719352b03bb1edd6b2
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-04-05 20:52:37 -0700

    attn_f outside block, noise gets orig + noisy part after attention on the features, impute*0.7 + recon*0.3 loss, skips1=skips1@attn_1, skipd2=skips2+attn_2@skips2, like SAITS, synth data history added 1-35

--- exe_agaid.py
+++ exe_agaid.py
@@ -83,7 +83,7 @@ config_dict_diffsaits = {
     'train': {
         'epochs': 5000,
         'batch_size': 16 ,
-        'lr': 1.0e-4
+        'lr': 1.0e-3
     },      
     'diffusion': {
         'layers': 4, 
