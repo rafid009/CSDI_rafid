e30e821eee17bacf16caa5700abdf68c2b9e5bf7
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-04-16 08:31:08 -0700

    nlayer=3, and diffsaits3 complete, xtilde1 + xtilde1@attn_f + Xnoise 1, attn_1 & 2, l=0.7l+0.3pred_i, more layers for attn_f, d_model=128, epoch=5000  1-25

--- exe_agaid.py
+++ exe_agaid.py
@@ -64,16 +64,16 @@ model_folder = "./saved_model"
 # if not os.path.isdir(model_folder):
 #     os.makedirs(model_folder)
 filename = 'model_csdi.pth'
-train(
-    model_csdi,
-    config_dict_csdi["train"],
-    train_loader,
-    valid_loader=valid_loader,
-    foldername=model_folder,
-    filename=filename
-)
+# train(
+#     model_csdi,
+#     config_dict_csdi["train"],
+#     train_loader,
+#     valid_loader=valid_loader,
+#     foldername=model_folder,
+#     filename=filename
+# )
 # nsample = 50
-# model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
+model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 # evaluate(model_csdi, valid_loader, nsample=nsample, scaler=1, foldername=model_folder)
 # model_folder_exp = "./saved_model_explode"
 # if not os.path.isdir(model_folder_exp):
