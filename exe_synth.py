e2a9771ea274523db17b670042162ef6f95d642d
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-04-17 14:55:33 -0700

    Trying out diff saits 4 1-12

--- exe_synth.py
+++ exe_synth.py
@@ -411,19 +411,19 @@ models = {
 mse_folder = "results_crps_mse_synth"
 data_folder = "results_synth_data"
 lengths = [20] #[10, 20, 50]
-for l in lengths:
-    print(f"length = {l}")
-    print(f"\nBlackout:\n")
-    evaluate_imputation_all(models=models, trials=10, mse_folder=mse_folder, dataset_name='synth', batch_size=16, length=l)
-    # evaluate_imputation(models, data_folder, length=l, trials=1, data=True)
-    print(f"\nForecasting:\n")
-    evaluate_imputation_all(models=models, trials=1, mse_folder=mse_folder, dataset_name='synth', batch_size=16, length=l, forecasting=True)
-    # evaluate_imputation(models, mse_folder=data_folder, length=l, forward_trial=True, trials=1, data=True)
+# for l in lengths:
+#     print(f"length = {l}")
+#     print(f"\nBlackout:\n")
+#     evaluate_imputation_all(models=models, trials=10, mse_folder=mse_folder, dataset_name='synth', batch_size=16, length=l)
+#     # evaluate_imputation(models, data_folder, length=l, trials=1, data=True)
+#     print(f"\nForecasting:\n")
+#     evaluate_imputation_all(models=models, trials=1, mse_folder=mse_folder, dataset_name='synth', batch_size=16, length=l, forecasting=True)
+#     # evaluate_imputation(models, mse_folder=data_folder, length=l, forward_trial=True, trials=1, data=True)
 
-miss_ratios = [0.2]#[0.2, 0.5, 0.8]
-for ratio in miss_ratios:
-    print(f"\nRandom Missing: ratio ({ratio})\n")
-    evaluate_imputation_all(models=models, trials=10, mse_folder=mse_folder, dataset_name='synth', batch_size=16, missing_ratio=ratio, random_trial=True)
+# miss_ratios = [0.2]#[0.2, 0.5, 0.8]
+# for ratio in miss_ratios:
+#     print(f"\nRandom Missing: ratio ({ratio})\n")
+#     evaluate_imputation_all(models=models, trials=10, mse_folder=mse_folder, dataset_name='synth', batch_size=16, missing_ratio=ratio, random_trial=True)
     # evaluate_imputation(models, mse_folder=data_folder, length=l, random_trial=True, trials=1, data=True, missing_ratio=ratio)
 # lengths = [20]
 print("For All")
