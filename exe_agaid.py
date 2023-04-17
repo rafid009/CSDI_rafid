ab73089b6285e0122884171ddddbbec800c40b56
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-04-16 18:00:06 -0700

    overall performance evaluation, agaid CSDI off 1-1

--- exe_agaid.py
+++ exe_agaid.py
@@ -64,14 +64,14 @@ model_folder = "./saved_model"
 # if not os.path.isdir(model_folder):
 #     os.makedirs(model_folder)
 filename = 'model_csdi.pth'
-train(
-    model_csdi,
-    config_dict_csdi["train"],
-    train_loader,
-    valid_loader=valid_loader,
-    foldername=model_folder,
-    filename=filename
-)
+# train(
+#     model_csdi,
+#     config_dict_csdi["train"],
+#     train_loader,
+#     valid_loader=valid_loader,
+#     foldername=model_folder,
+#     filename=filename
+# )
 # nsample = 50
 model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 print(f"CSDI params: {get_num_params(model_csdi)}")
