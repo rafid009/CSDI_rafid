2f5eaf98604273f78d2fd86c45814b7da623aec0
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-04-19 07:59:32 -0700

    ds3 old stable for synth eval 1-3

--- diff_models.py
+++ diff_models.py
@@ -407,21 +407,21 @@ class diff_SAITS_3(nn.Module):
         skips_tilde_1 = torch.zeros_like(enc_output)
         for encoder_layer in self.layer_stack_for_first_block:
             # new time add
-            enc_output, skip, attn_weights = encoder_layer(enc_output, pos_cond, diff_emb)
+            # enc_output, skip, attn_weights = encoder_layer(enc_output, pos_cond, diff_emb)
             # old stable better
-            # enc_output, skip, _ = encoder_layer(enc_output, pos_cond, diff_emb)
+            enc_output, skip, _ = encoder_layer(enc_output, pos_cond, diff_emb)
             skips_tilde_1 += skip
         skips_tilde_1 /= math.sqrt(len(self.layer_stack_for_first_block))
         skips_tilde_1 = self.reduce_skip_z(skips_tilde_1)
         
         # new time add
-        attn_weights = attn_weights.squeeze(dim=1)  # namely term A_hat in Eq.
-        if len(attn_weights.shape) == 4:
-            # if having more than 1 head, then average attention weights from all heads
-            attn_weights = torch.transpose(attn_weights, 1, 3)
-            attn_weights = attn_weights.mean(dim=3)
-            attn_weights = torch.transpose(attn_weights, 1, 2)
-            attn_weights_f = torch.softmax(attn_weights_f, dim=-1)
+        # attn_weights = attn_weights.squeeze(dim=1)  # namely term A_hat in Eq.
+        # if len(attn_weights.shape) == 4:
+        #     # if having more than 1 head, then average attention weights from all heads
+        #     attn_weights = torch.transpose(attn_weights, 1, 3)
+        #     attn_weights = attn_weights.mean(dim=3)
+        #     attn_weights = torch.transpose(attn_weights, 1, 2)
+        #     attn_weights_f = torch.softmax(attn_weights_f, dim=-1)
 
         # Feature attention added
         attn_weights_f = attn_weights_f.squeeze(dim=1)  # namely term A_hat in Eq.
@@ -435,12 +435,12 @@ class diff_SAITS_3(nn.Module):
         # Old stable better
         X_tilde_1 = X_tilde_1 @ attn_weights_f + X_tilde_1
         # new time add
-        X_tilde_1 = torch.transpose(X_tilde_1, 1, 2)
-        X_tilde_1 = X_tilde_1 @ attn_weights + X_tilde_1
-        X_tilde_1 = torch.transpose(X_tilde_1, 1, 2)
+        # X_tilde_1 = torch.transpose(X_tilde_1, 1, 2)
+        # X_tilde_1 = X_tilde_1 @ attn_weights + X_tilde_1
+        # X_tilde_1 = torch.transpose(X_tilde_1, 1, 2)
 
         # Old stable better
-        # X_tilde_1 = X_tilde_1 + X[:, 1, :, :] 
+        X_tilde_1 = X_tilde_1 + X[:, 1, :, :] 
 
         # second DMSA block
 
