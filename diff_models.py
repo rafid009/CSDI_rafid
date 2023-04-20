39b7607844f0b21c10744c7616157171d577e7db
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-04-19 19:40:26 -0700

    test synth attn1 without final 1-4

--- diff_models.py
+++ diff_models.py
@@ -221,8 +221,8 @@ class ResidualEncoderLayer_2(nn.Module):
         # self.enc_layer_1 = EncoderLayer(d_time, actual_d_feature, channels, d_inner, n_head, d_k, d_v, dropout, 0,
         #                  diagonal_attention_mask)
         # combi 2
-        self.enc_layer_1 = EncoderLayer(d_time, actual_d_feature, channels, d_inner, n_head, d_k, d_v, dropout, 0,
-                         diagonal_attention_mask)
+        # self.enc_layer_1 = EncoderLayer(d_time, actual_d_feature, channels, d_inner, n_head, d_k, d_v, dropout, 0,
+        #                  diagonal_attention_mask)
         
         self.enc_layer_2 = EncoderLayer(d_time, actual_d_feature, 2 * channels, d_inner, n_head, d_k, d_v, dropout, 0,
                          diagonal_attention_mask)
@@ -276,12 +276,12 @@ class ResidualEncoderLayer_2(nn.Module):
         
 
         diff_proj = self.diffusion_projection(diffusion_emb).unsqueeze(-1)
-        y = x_proj + diff_proj + cond
+        y = x_proj + diff_proj #+ cond
 
-
-        y = torch.transpose(y, 1, 2) # (B, K, channels)
-        y, _ = self.enc_layer_1(y)
-        y = torch.transpose(y, 1, 2)
+        # attn1
+        # y = torch.transpose(y, 1, 2) # (B, K, channels)
+        # y, _ = self.enc_layer_1(y)
+        # y = torch.transpose(y, 1, 2)
 
 
         y = self.conv_layer(y)
