eb9375a4ed5285dcf9eb8517370eaa045da623b8
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-04-26 16:35:10 -0700

    removed attention encoder 1 1-1

--- exe_synth.py
+++ exe_synth.py
@@ -386,7 +386,7 @@ config_dict_diffsaits = {
 print(f"config: {config_dict_diffsaits}")
 model_diff_saits = CSDI_Synth(config_dict_diffsaits, device, target_dim=len(given_features)).to(device)
 
-filename = "model_diffsaits_synth_qual.pth"
+filename = "model_diffsaits_synth_qual_attn1.pth"
 print(f"\n\DiffSAITS training starts.....\n")
 # model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 train(
@@ -409,8 +409,8 @@ models = {
     'SAITS': saits,
     'DiffSAITS': model_diff_saits
 }
-mse_folder = "results_synth_qual_2"
-data_folder = "results_synth_qual_2"
+mse_folder = "results_synth_qual_no_attn1"
+data_folder = "results_synth_qual_no_attn1"
 lengths = [20, 50, 80]
 for l in lengths:
     print(f"\nlength = {l}")
