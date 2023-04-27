0f0b68cbde714fba0d24399ad7289e70e6571b29
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-04-27 10:43:57 -0700

    added cond_X in the second block's attn_f intigration instead of X[1] 1-1

--- diff_models.py
+++ diff_models.py
@@ -422,7 +422,7 @@ class diff_SAITS_3(nn.Module):
         X_tilde_1 = self.reduce_dim_z(enc_output)
 
         # Feature encode for second block
-        X_tilde_1 = X_tilde_1 @ attn_weights_f + X[:, 1, :, :] #+ X_tilde_1
+        X_tilde_1 = X_tilde_1 @ attn_weights_f + cond_X #X[:, 1, :, :] #+ X_tilde_1
 
 
         # Old stable better
--- exe_synth.py
+++ exe_synth.py
@@ -386,7 +386,7 @@ config_dict_diffsaits = {
 print(f"config: {config_dict_diffsaits}")
 model_diff_saits = CSDI_Synth(config_dict_diffsaits, device, target_dim=len(given_features)).to(device)
 
-filename = "model_diffsaits_synth_no_softmax.pth"
+filename = "model_diffsaits_synth_condX_2nd_block.pth"
 print(f"\n\DiffSAITS training starts.....\n")
 # model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 train(
@@ -409,8 +409,8 @@ models = {
     'SAITS': saits,
     'DiffSAITS': model_diff_saits
 }
-mse_folder = "results_synth_qual_no_softmax"
-data_folder = "results_synth_qual_no_softmax"
+mse_folder = "results_synth_qual_condX_2nd_blockx"
+data_folder = "results_synth_qual_condX_2nd_block"
 lengths = [20, 50, 80]
 for l in lengths:
     print(f"\nlength = {l}")
