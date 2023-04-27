f371428cd10dfa56ff4389bb9a0673892cfc7c35
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-04-27 16:44:16 -0700

    condX + X[1] into first embedding before the 1st block 1-1

--- diff_models.py
+++ diff_models.py
@@ -388,7 +388,7 @@ class diff_SAITS_3(nn.Module):
         # input_X_for_first = self.embedding_1(input_X_for_first)
 
         # combi 2
-        input_X_for_first = torch.cat([cond_X, masks[:,1,:,:]], dim=2)
+        input_X_for_first = torch.cat([cond_X + X[:, 1, :, :], masks[:,1,:,:]], dim=2)
         input_X_for_first = self.embedding_1(input_X_for_first)
 
 
--- exe_synth.py
+++ exe_synth.py
@@ -349,7 +349,7 @@ saits = pickle.load(open(saits_model_file, 'rb'))
 
 config_dict_diffsaits = {
     'train': {
-        'epochs':3500, # 3000 -> ds3
+        'epochs':3000, # 3000 -> ds3
         'batch_size': 16 ,
         'lr': 1.0e-3
     },      
@@ -386,7 +386,7 @@ config_dict_diffsaits = {
 print(f"config: {config_dict_diffsaits}")
 model_diff_saits = CSDI_Synth(config_dict_diffsaits, device, target_dim=len(given_features)).to(device)
 
-filename = "model_diffsaits_synth_condX_2nd_block.pth"
+filename = "model_diffsaits_synth_condX_X1_before_1st_block.pth"
 print(f"\n\DiffSAITS training starts.....\n")
 # model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 train(
@@ -409,8 +409,8 @@ models = {
     'SAITS': saits,
     'DiffSAITS': model_diff_saits
 }
-mse_folder = "results_synth_qual_condX_2nd_blockx"
-data_folder = "results_synth_qual_condX_2nd_block"
+mse_folder = "results_synth_qual_condX_X1_before_1st_block"
+data_folder = "results_synth_qual_condX_X1_before_1st_block"
 lengths = [20, 50, 80]
 for l in lengths:
     print(f"\nlength = {l}")
