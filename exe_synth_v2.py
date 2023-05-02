0e8daf965e2fab79f2ca88bbd876e3c966024243
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-05-02 09:24:21 -0700

    corrected tan of synth_v2 1-2

--- exe_synth_v2.py
+++ exe_synth_v2.py
@@ -316,7 +316,7 @@ nsample = 50
 n_steps = 100
 n_features = len(given_features)
 num_seasons = 32
-noise = True
+noise = False
 train_loader, valid_loader = get_dataloader(n_steps, n_features, num_seasons, batch_size=16, missing_ratio=0.1, seed=10, is_test=False, v2=True, noise=noise)
 
 model_csdi = CSDI_Synth(config_dict_csdi, device, target_dim=len(given_features)).to(device)
